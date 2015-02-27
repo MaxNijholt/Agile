49a486aa6445e5d26f2723317456d01b2c662a72
sromer <info@stephanromer.nl>
2015-02-27 11:53:55 +0100

    Voorbeeld PDO database Classe

---
+++ php-examples/database/Singleton.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace tool;
+
+/**
+ * Abstract class which can be inherited (extended) and it will prevent
+ * the parent class from reinstantiating itself, so only one instance allowed.
+ */
+abstract class Singleton {
+	protected static $instances = array();
+
+	static function getInstance() {
+		$class = get_called_class();
+		if(!isset(self::$instances[$class]))
+		{
+			$r = new \ReflectionClass($class);
+   			self::$instances[$class] = $r->newInstanceArgs(func_get_args());
+		}
+		return self::$instances[$class];
+	}
+
+	final protected function __clone() { }
+}
