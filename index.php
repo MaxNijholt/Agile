badfdc964bf861174527c67da572109ad71894e4
Max Nijholt <m.nijholt@student.avans.nl>
2015-03-11 13:49:36 +0100

    Foutje bedankt. Merge ging niet zoals bedoeld was.

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+// TEMP
+session_start();
+error_reporting(E_ALL);
+ini_set("display_errors",1);
+// Before removing
+// -	check all TODO
+// -	check all TEMP
+// /TEMP
+
+
+include 'framework/autoload.php';
+
+try {
+	$p = new core\Page();
+	$p->setURL(@$_GET['q']);
+	$p->build();
+} catch (Exception $e) {
+	echo '<pre>';
+	print_r($e);
+	echo '</pre>';
+}
