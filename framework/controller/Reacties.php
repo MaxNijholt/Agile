65e1ff795009c15491059b51edcb2d5b148348dd
Toine Bakkeren <toine.bakkeren@gmail.com>
2015-05-28 12:46:39 +0200

    - Comments worden opgehaald uit de database op basis van het content_ID

---
+++ framework/controller/Reacties.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: toinebakkeren
+ * Date: 28/05/15
+ * Time: 11:11
+ */
+
+namespace controller;
+use core;
+
+class Reacties extends core\Controller {
+
+    public function index(){
+
+    }
+
+
+
+}
