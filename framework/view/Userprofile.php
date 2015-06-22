cac841aeb5f8c4472b3c5a93eb3efcbda0d9433a
Max Nijholt <m.nijholt@student.avans.nl>
2015-06-22 11:18:46 +0200

    User profile data protection and comments on methods

--- framework/view/Userprofile.php
+++ framework/view/Userprofile.php
@@ -33,20 +33,20 @@
 				<h2 class="form-signin-heading">Gebruikersprofiel van: <?php echo $user->firstname." ".$user->lastname; ?></h2>
 				<h3 class="form-signin-heading"><?php echo $user->postalcode." op ".$user->housenumber; ?></h3>
     			<label for="inputVoornaam" class="sr-only">Voornaam</label>
-				<input type="text" name="inputFirstName" placeholder="<?php echo $user->firstname; ?>" id="inputFirstname" class="form-control" required="required" title="">
+				<input type="text" name="inputFirstName" placeholder="<?php echo $user->firstname; ?>" id="inputFirstname" class="form-control" title="">
 
 				<label for="inputAchternaam" class="sr-only">Achternaam</label>
-				<input type="text" name="inputLastName" placeholder="<?php echo $user->lastname; ?>" id="inputLastname" class="form-control" required="required" title="">
+				<input type="text" name="inputLastName" placeholder="<?php echo $user->lastname; ?>" id="inputLastname" class="form-control" title="">
 
 				<label for="inputEmail" class="sr-only">E-Mail adres</label>
-				<input type="email" name="inputEMail" placeholder="<?php echo $user->email; ?>" id="inputEmail" class="form-control" value="" required="required" title="">
+				<input type="email" name="inputEMail" placeholder="<?php echo $user->email; ?>" id="inputEmail" class="form-control" value="" title="">
 				<label for="inputEmailConfirm" class="sr-only">E-Mail adres bevestiging</label>
-				<input type="email" name="inputEMailConfirm" placeholder="E-Mail bevestiging" id="inputEmailConfirm" class="form-control" value="" required="required" title="">
+				<input type="email" name="inputEMailConfirm" placeholder="E-Mail bevestiging" id="inputEmailConfirm" class="form-control" value="" title="">
 
     			<label for="inputPassword" class="sr-only">Wachtwoord</label>
-				<input type="password" name="inputPassword" placeholder="Wachtwoord" id="inputPassword" class="form-control" required="required" title="">
+				<input type="password" name="inputPassword" placeholder="Wachtwoord" id="inputPassword" class="form-control" title="">
         		<label for="inputPasswordConfirm" class="sr-only">Bevestig wachtwoord</label>
-				<input type="password" name="inputPasswordConfirm" placeholder="Wachtwoord bevestiging" id="inputPasswordConfirm" class="form-control" required="required" title="" onchange="checkPass();">
+				<input type="password" name="inputPasswordConfirm" placeholder="Wachtwoord bevestiging" id="inputPasswordConfirm" class="form-control" title="" onchange="checkPass();">
         		<div id="passerror" class="alert alert-warning" style="display:none;">
             	<a href="#" class="close" data-dismiss="alert">&times;</a>
             	<strong>Error!</strong> Wachtwoorden komen niet overeen.
